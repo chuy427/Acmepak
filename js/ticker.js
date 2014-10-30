81b38713934f89bfbd653fa7af251ed9b3c2106c
Jesus Gonzalez <chuy427jg@gmail.com>
2014-10-30 14:52:18 -0700

    fixed navigation issues and identation

--- js/ticker.js
+++ js/ticker.js
@@ -3,10 +3,10 @@ var height = jQuery('.ticker').height();
 var numberDivs = jQuery('.ticker').children().length; 
 var first = jQuery('.ticker h1:nth-child(1)'); 
 setInterval(function() {
-    var number = current * -height;
-    first.css('margin-top', number + 'px');
-    if (current === numberDivs) {
-        first.css('margin-top', '0px');
-        current = 1;
-    } else current++;
+  var number = current * -height;
+  first.css('margin-top', number + 'px');
+  if (current === numberDivs) {
+      first.css('margin-top', '0px');
+      current = 1;
+  } else current++;
 }, 2500);
